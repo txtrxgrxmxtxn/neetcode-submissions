3766e267bf219b8bd4d64fa9f03648eed5289c39
txtrxgrxmxtxn <280268covarrubiashernandez@gmail.com>
2026-09-23 20:29:09 -0600

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,27 @@
+class Solution: 
+    def findDuplicate(self, nums: List[int])-> int:
+        # step 1: find matching point: 
+        turtle = nums[0]
+        rabbit = nums[0]
+
+
+        while True: 
+            turtle = nums[turtle] #1 step
+            rabbit = nums[nums[rabbit]] #2 steps
+
+            if turtle == rabbit:
+                break 
+
+
+        
+        #step 2: find cycle entrance
+        turtle = nums[0]
+
+        while turtle != rabbit: 
+            turtle = nums[turtle]
+            rabbit = nums[rabbit]
+
+
+        return turtle 
+
+        
